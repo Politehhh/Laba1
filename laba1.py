c3633fea18d37d21190cbb782693b83b94a035b3
Politehhh <suleymangulushov@gmail.com>
2025-09-19 10:01:01 +0300

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,45 +1,59 @@
 class InternetShop:
-    def __init__(self, store_name, tv_price, purchases):
-        self._store_name = store_name
-        self.tv_price = tv_price  # проверка в setter
-        self.purchases = purchases  # проверка в setter
+    def __init__(self, name: str, price: float, count: int):
+        self.set_shop_name(name)
+        self.set_tv_price(price)
+        self.set_purchase_count(count)
 
-    @property
-    def store_name(self):
-        return self._store_name
+    def set_shop_name(self, name: str):
+        if not name.strip():
+            raise ValueError("У магазина должно быть название")
+        self._shop_name = name
 
-    @property
-    def tv_price(self):
+    def set_tv_price(self, price: float):
+        if price <= 0:
+            raise ValueError("Цена телевизора меньше 0, повышай")
+        if price > 300_000:
+            raise ValueError("Цена телевизора слишком дорогая, понижай")
+        self._tv_price = price
+
+    def set_purchase_count(self, count: int):
+        if count < 0:
+            raise ValueError("Товары не могут быть 0 или меньше 0")
+        if count > 100:
+            raise ValueError("Товаров может быть только 100, не больше")
+        self._purchase_count = count
+
+    def get_shop_name(self) -> str:
+        return self._shop_name
+
+    def get_tv_price(self) -> float:
         return self._tv_price
 
-    @tv_price.setter
-    def tv_price(self, value):
-        if value > 0:
-            self._tv_price = value
-        else:
-            print("Ошибка: цена должна быть положительной! Установлено значение 1.")
-            self._tv_price = 1
+    def get_purchase_count(self) -> int:
+        return self._purchase_count
 
-    @property
-    def purchases(self):
-        return self._purchases
+    def calc_revenue(self) -> float:
+        return self._tv_price * self._purchase_count
 
-    @purchases.setter
-    def purchases(self, value):
-        if value >= 0:
-            self._purchases = value
-        else:
-            print("Ошибка: количество покупок не может быть отрицательным! Установлено значение 0.")
-            self._purchases = 0
+    def set_revenue_only(self, revenue: float):
+        if revenue < 0:
+            raise ValueError("Меньше нуля нельзя заработать с продажи")
+            
+def main():
+    try:
+        shop = InternetShop("PoliTechMarket", 300_000, 100)
 
-    def get_revenue(self):
-        return self._tv_price * self._purchases
+        print(f"Магазин: {shop.get_shop_name()}")
+        print(f"Цена телевизора: {shop.get_tv_price():.0f} руб.")
+        print(f"Количество покупок: {shop.get_purchase_count()}")
+        print(f"Общая выручка: {shop.calc_revenue():.0f} руб.")
+
+        shop.set_revenue_only(300_000)
+
+    except ValueError as e:
+        print("Одна ошибка, и ты ошибся")
+        print(e)
 
 
-# Данные заданы в коде
-shop = InternetShop("BestTV", 1200.5, 15)
-
-print("Магазин:", shop.store_name)
-print("Цена телевизора:", shop.tv_price, "BYN")
-print("Количество покупок:", shop.purchases)
-print("Общая выручка:", shop.get_revenue(), "BYN")
+if __name__ == "__main__":
+    main()
